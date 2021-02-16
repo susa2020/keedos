da4ad0fab4e23a0b16d28812795e8770aba6c0f0
brianchou452 <brianchou452@gmail.com>
2021-02-16 22:40:29 +0800

    change user management system
    https://github.com/msaad1999/PHP-Login-System
    
    已和現有檔案合併
    尚未編寫檔案架構

---
+++ assets/setup/env.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined('APP_NAME'))                       define('APP_NAME', 'keedos');
+if (!defined('APP_ORGANIZATION'))               define('APP_ORGANIZATION', 'susa');
+if (!defined('APP_OWNER'))                      define('APP_OWNER', 'msaad1999');
+if (!defined('APP_DESCRIPTION'))                define('APP_DESCRIPTION', 'Embeddable PHP Login System');
+
+if (!defined('ALLOWED_INACTIVITY_TIME'))        define('ALLOWED_INACTIVITY_TIME', time()+1*60);
+
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'klik_loginsystem');
+if (!defined('DB_HOST'))                        define('DB_HOST','localhost');
+if (!defined('DB_USERNAME'))                    define('DB_USERNAME','keedos');
+if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD' ,'mB7[rUl!s)');
+if (!defined('DB_PORT'))                        define('DB_PORT' ,'');
+
+if (!defined('MAIL_HOST'))                      define('MAIL_HOST', 'smtp.gmail.com');
+if (!defined('MAIL_USERNAME'))                  define('MAIL_USERNAME', 'example.email@gmail.com');
+if (!defined('MAIL_PASSWORD'))                  define('MAIL_PASSWORD', 'example-password');
+if (!defined('MAIL_ENCRYPTION'))                define('MAIL_ENCRYPTION', 'ssl');
+if (!defined('MAIL_PORT'))                      define('MAIL_PORT', 465);
