7096f9fdfc360b2b70beebb3aab707a425980420
brianchou452 <brianchou452@gmail.com>
2021-02-16 23:23:47 +0800

    change login
    login/index.php
    index old.php 為原始檔

--- login/index.php
+++ login/index.php
@@ -1,3 +1,4 @@
+
 <?php
 
 define('TITLE', "Login");
@@ -5,24 +6,33 @@ include '../assets/layouts/header.php';
 check_logged_out();
 ?>
 
+<html>
 
-<div class="container">
-    <div class="row">
-        <div class="col-sm-4">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>keedos</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Search.css">
+    <link rel="stylesheet" href="assets/css/smoothproducts.css">
+</head>
 
-        </div>
-        <div class="col-sm-4">
-            <form class="form-auth" action="includes/login.inc.php" method="post">
+<body>
+    <section class="clean-block clean-form dark">
+        <div class="container">
+            <div class="block-heading">
+                <h2 class="text-info">登入</h2>
+                <p>歡迎回來!</p>
+            </div>
+            <form action="includes/login.inc.php" method="post">
 
                 <?php insert_csrf_token(); ?>
 
-                <div class="text-center">
-                    <img class="mb-1" src="../assets/images/logo.png" alt="" width="130" height="130">
-                </div>
-
-                <h6 class="h3 mb-3 font-weight-normal text-muted text-center">Login to your Account</h6>
-
-                <div class="text-center mb-3">
+                <div class="text-info">
                     <small class="text-success font-weight-bold">
                         <?php
                             if (isset($_SESSION['STATUS']['loginstatus']))
@@ -33,58 +43,84 @@ check_logged_out();
                 </div>
 
                 <div class="form-group">
-                    <label for="username" class="sr-only">Username</label>
-                    <input type="text" id="username" name="username" class="form-control" placeholder="Username" required autofocus>
-                    <sub class="text-danger">
-                        <?php
-                            if (isset($_SESSION['ERRORS']['nouser']))
-                                echo $_SESSION['ERRORS']['nouser'];
-                        ?>
-                    </sub>
+                  <label for="username" class="sr-only">Username</label>
+                  <input type="text" id="username" name="username" class="form-control" placeholder="Username" required autofocus>
+                  <sub class="text-danger">
+                      <?php
+                          if (isset($_SESSION['ERRORS']['nouser']))
+                              echo $_SESSION['ERRORS']['nouser'];
+                      ?>
+                  </sub>
                 </div>
-
                 <div class="form-group">
-                    <label for="password" class="sr-only">Password</label>
-                    <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
-                    <sub class="text-danger">
-                        <?php
-                            if (isset($_SESSION['ERRORS']['wrongpassword']))
-                                echo $_SESSION['ERRORS']['wrongpassword'];
-                        ?>
-                    </sub>
+                  <label for="password" class="sr-only">Password</label>
+                  <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
+                  <sub class="text-danger">
+                      <?php
+                          if (isset($_SESSION['ERRORS']['wrongpassword']))
+                              echo $_SESSION['ERRORS']['wrongpassword'];
+                      ?>
+                  </sub>
                 </div>
-
-                <div class="col-auto my-1 mb-4">
-                    <div class="custom-control custom-checkbox mr-sm-2">
-                        <input type="checkbox" class="custom-control-input" id="rememberme" name="rememberme">
-                        <label class="custom-control-label" for="rememberme">Remember me</label>
+                <div class="form-group">
+                    <div class="form-check">
+                      <input type="checkbox" class="custom-control-input" id="rememberme" name="rememberme">
+                      <label class="custom-control-label" for="rememberme">Remember me</label>
                     </div>
                 </div>
-
-                <button class="btn btn-lg btn-primary btn-block" type="submit" value="loginsubmit" name="loginsubmit">Login</button>
-
-                <p class="mt-3 text-muted text-center"><a href="../reset-password/">forgot password?</a></p>
-
-                <p class="mt-4 mb-3 text-muted text-center">
-                    <a href="https://github.com/msaad1999/PHP-Login-System" target="_blank">
-                        Login System
-                    </a> | 
-                    <a href="https://github.com/msaad1999/PHP-Login-System/blob/master/LICENSE" target="_blank">
-                        MIT License
-                    </a>
-                </p>
-                
+                <button class="btn btn-primary btn-block" type="submit" value="loginsubmit" name="loginsubmit" >Log In</button>
             </form>
+            <div class="block-heading">
+                <h2 class="text-info"></h2>
+            </div>
         </div>
-        <div class="col-sm-4">
-
+    </section>
+    <footer class="page-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h5>Get started</h5>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Sign up</a></li>
+                        <li><a href="#">Downloads</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <h5>About us</h5>
+                    <ul>
+                        <li><a href="#">Company Information</a></li>
+                        <li><a href="#">Contact us</a></li>
+                        <li><a href="#">Reviews</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <h5>Support</h5>
+                    <ul>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Help desk</a></li>
+                        <li><a href="#">Forums</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <h5>Legal</h5>
+                    <ul>
+                        <li><a href="#">Terms of Service</a></li>
+                        <li><a href="#">Terms of Use</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
+        <div class="footer-copyright">
+            <p>© 2021 Copyright Text</p>
+        </div>
+    </footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+    <script src="assets/js/smoothproducts.min.js"></script>
+    <script src="assets/js/theme.js"></script>
+</body>
 
-
-<?php
-
-include '../assets/layouts/footer.php'
-
-?>
+</html>
