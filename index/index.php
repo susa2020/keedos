977aa1a3314b3217702235f86268ca142af22593
linqiaoyi213 <linqiaoyi213@gmail.com>
2021-02-18 15:50:53 +0800

    index.php

---
+++ index/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<?php
+
+define('TITLE', "Login");
+include '../assets/layouts/header.php';
+check_logged_out();
+?>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>keedos official</title>
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="../assets/css/Login-Form-Clean.css">
+    <link rel="stylesheet" href="../assets/css/Registration-Form-with-Photo.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+     <script src="jquery.js"></script>
+</head>
+
+<body>
+   
+
+
+</body>
+
+</html>
