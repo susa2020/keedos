2abd0d1e79a7d0870f2287120550e57a1419eb5f
brianchou452 <brianchou452@gmail.com>
2021-03-27 01:00:37 +0800

    add lionfree config backup

---
+++ env.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined('APP_NAME'))                       define('APP_NAME', 'keedos');
+if (!defined('APP_ORGANIZATION'))               define('APP_ORGANIZATION', 'susa');
+if (!defined('APP_OWNER'))                      define('APP_OWNER', 'susa');
+if (!defined('APP_DESCRIPTION'))                define('APP_DESCRIPTION', 'keedos');
+
+if (!defined('ALLOWED_INACTIVITY_TIME'))        define('ALLOWED_INACTIVITY_TIME', time()+1*60);
+
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'susalio1_keedos');
+if (!defined('DB_HOST'))                        define('DB_HOST','192.168.2.200');
+if (!defined('DB_USERNAME'))                    define('DB_USERNAME','susalio1_keedos');
+if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD' ,'HPHGUaFmIV');
+if (!defined('DB_PORT'))                        define('DB_PORT' ,'');
+
+if (!defined('MAIL_HOST'))                      define('MAIL_HOST', 'smtp.gmail.com');
+if (!defined('MAIL_USERNAME'))                  define('MAIL_USERNAME', 'groupsusa2020@gmail.com');
+if (!defined('MAIL_PASSWORD'))                  define('MAIL_PASSWORD', 'susasusa');
+if (!defined('MAIL_ENCRYPTION'))                define('MAIL_ENCRYPTION', 'ssl');
+if (!defined('MAIL_PORT'))                      define('MAIL_PORT', 465);
---
+++ settings.ini.php
@@ -0,0 +1,6 @@
+;<?php return; ?>
+[SQL]
+host = 192.168.2.200
+user = susalio1_keedos
+password = "HPHGUaFmIV"
+dbname = susalio1_keedos
