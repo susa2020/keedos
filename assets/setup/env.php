80ad2c9633d093b39480f88c3a0f4e247f4b5c7f
brianchou452 <brianchou452@gmail.com>
2021-04-18 22:01:56 +0800

    add email

--- assets/setup/env.php
+++ assets/setup/env.php
@@ -15,6 +15,6 @@ if (!defined('DB_PORT'))                        define('DB_PORT' ,'');
 
 if (!defined('MAIL_HOST'))                      define('MAIL_HOST', 'smtp.gmail.com');
 if (!defined('MAIL_USERNAME'))                  define('MAIL_USERNAME', 'groupsusa2020@gmail.com');
-if (!defined('MAIL_PASSWORD'))                  define('MAIL_PASSWORD', 'susasusa');
+if (!defined('MAIL_PASSWORD'))                  define('MAIL_PASSWORD', 'Susa20200420?');
 if (!defined('MAIL_ENCRYPTION'))                define('MAIL_ENCRYPTION', 'ssl');
 if (!defined('MAIL_PORT'))                      define('MAIL_PORT', 465);
